45ee2e3fa55bfa214674ee46bb819ca51fcd1cf1
sahlannaufal <sahlannaufal.sn@gmail.com>
2024-07-12 21:17:12 +0700

    auth

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,19 +1,50 @@
+import Hash from '@ioc:Adonis/Core/Hash'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class AuthController {
 
     async login({auth, request, response}: HttpContextContract) {
-        try {
-            const {email, password} = request.body()
+        // try {
+        //     const {email, password} = request.body()
 
-            const token = await auth.use('api').attempt(email, password)
+        //     const token = await auth.use('api').attempt(email, password)
 
-            return {
+        //     return {
+        //         token
+        //     }
+        // } catch (error) {
+        //     response.unauthorized('')
+        // }
+
+
+        const { email, password } = request.only(['email', 'password'])
+
+        // Mencari user berdasarkan email
+        const user = await User.findBy('email', email)
+        if (!user) {
+            return response.status(422).json({
+                message: 'Email tidak terdaftar!'
+            })
+        }
+
+        // Memeriksa kecocokan password
+        const isPasswordValid = await Hash.verify(user.password, password)
+        if (!isPasswordValid) {
+            return response.status(422).json({
+                message: 'Password salah!'
+            })
+        }
+
+        // Membuat token API
+        const token = await auth.use('api').generate(user)
+
+        // Mengembalikan respons
+        return response.json({
+            data: {
+                user,
                 token
             }
-        } catch (error) {
-            response.unauthorized('')
-        }
+        })
     }
 }
